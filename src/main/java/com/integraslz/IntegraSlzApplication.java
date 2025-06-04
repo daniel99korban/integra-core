51ed7026fa2b83327a11a0be0ec4b4065f92c4cb
apollw <apollwl@gmail.com>
2025-06-04 14:22:29 -0300

    Initial Commit

---
+++ src/main/java/com/integraslz/IntegraSlzApplication.java
@@ -0,0 +1,13 @@
+package com.integraslz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegraSlzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegraSlzApplication.class, args);
+	}
+
+}

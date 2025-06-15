c76aebc224b3ebf56617c39c7d4266c8d9d37663
apollw <apollw@gmail.com>
2025-06-15 09:41:36 -0300

    feat: UserController criado

--- src/main/java/com/integraslz/api/UserController.java
+++ src/main/java/com/integraslz/api/UserController.java
@@ -1,5 +1,33 @@
 package com.integraslz.api;
 
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.*;
+import com.integraslz.application.User.UserService;
+import com.integraslz.domain.Entity.User;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
-    String nome;
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/by-email")
+    public Optional<User> findByEmail(@RequestParam String email) {
+        return service.findByEmail(email);
+    }
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return service.findOrCreate(
+                user.getName(),
+                user.getEmail(),
+                user.getProvider(),
+                user.getProviderId()
+        );
+    }
 }
